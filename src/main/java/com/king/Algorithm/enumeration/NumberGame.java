b80b1f35ecc13df4eb7cfd17dc392922c3176728
riyvitxgt <zhukangmin@hotmail.com>
2014-09-19 12:05:46 +0800

    添加枚举算法

--- src/main/java/com/king/Algorithm/enumeration/NumberGame.java
+++ src/main/java/com/king/Algorithm/enumeration/NumberGame.java
@@ -2,24 +2,31 @@ package com.king.Algorithm.enumeration;
 
 /**
  * 完成这样一个猜字游戏
- * 	算法问题
- * X    算
+ * 	算法的问题
+ * X     算
  * －－－－－－
- * 题题题题题
+ * 题题题题题题
  * @author King
  *
  */
 
 public class NumberGame {
 	public int result;
+	public int num;
 	public void find(){
-		for(int f1 = 1; f1 < 9;f1++){
-			for(int f2 = 0; f2 < 9; f2++){
-				for(int f3 = 0; f3 < 9; f3++){
-					for(int f4 = 0; f4 < 9; f4++){
-						for( int f5 = 1; f5 < 9; f5++){
-							result = f5*10000 + f5*1000 + f5*100 + f5*10 + f5;
-							
+		for(int f1 = 1; f1 <= 9;f1++){
+			for(int f2 = 0; f2 <= 9; f2++){
+				for(int f3 = 0; f3 <= 9; f3++){
+					for(int f4 = 0; f4 <= 9; f4++){
+						for( int f5 = 1; f5 <= 9; f5++){
+							result = f5*111111;
+							num = f1*10000 + f2*1000 + f3*100 + f4*10 + f5;
+							if(result == (num*f1)){
+								System.out.println(" " + num);
+								System.out.println("X    " + f1);
+								System.out.println("-------");
+								System.out.println(result);
+							}
 						}
 					}
 				}
---
+++ src/test/java/com/king/Algorithm/enumeration/NumberGameTest.java
@@ -0,0 +1,15 @@
+package com.king.Algorithm.enumeration;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NumberGameTest {
+
+	@Test
+	public void testFinde() {
+		NumberGame ng = new NumberGame();
+		ng.find();
+	}
+
+}
